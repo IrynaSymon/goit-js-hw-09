dd7306e19252047ec52a12bdaa4015f8948e903d
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-07-23 11:14:24 +0000

    Deploying to gh-pages from @ IrynaSymon/goit-js-hw-09@a92c668dc4340f836ca16f031fbdd71db6471382 🚀

---
+++ 01-color-switcher.0a45558d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}));var o=null}();
+//# sourceMappingURL=01-color-switcher.0a45558d.js.map
---
+++ 01-color-switcher.e1807434.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}));let r=null;
+//# sourceMappingURL=01-color-switcher.e1807434.js.map
