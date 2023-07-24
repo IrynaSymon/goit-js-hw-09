ea0dea2fea3966888585c38b7ca844f406979229
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-07-24 13:55:33 +0200

    corekta

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,18 +5,17 @@ const bodyRef = document.querySelector("body");
 btnStart.addEventListener("click", onStart);
 btnStop.addEventListener("click", onStop);
 let timerIdInt = null;
-btnStop.disabled = true;
+btnStart.disabled = true;
 function onStart() {
-    btnStart.disabled = true;  
+    timerIdInt = setInterval(() => { bodyRef.style.backgroundColor = getRandomHexColor() }, 1000);
+    btnStart.disabled = true;
     btnStop.disabled = false;
-timerIdInt = setInterval(() => { bodyRef.style.backgroundColor = getRandomHexColor() }, 1000);
-//  btnStart.toggleAttribute("disabled",true);   
 }  
 function onStop() {
-    btnStart.disabled = false;  
+    btnStart.disabled = false;
     btnStop.disabled = true;
 clearInterval(timerIdInt);
-// btnStart.removeAttribute("disabled");
+
 }
 function getRandomHexColor() {
 return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
