a085f6950ffb1b280dfffb859c0dce81409f8a04
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-07-24 11:56:02 +0000

    Deploying to gh-pages from @ IrynaSymon/goit-js-hw-09@ea0dea2fea3966888585c38b7ca844f406979229 🚀

--- 01-color-switcher.f0e7999f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}));var d=null;e.disabled=!0}();
-//# sourceMappingURL=01-color-switcher.f0e7999f.js.map
